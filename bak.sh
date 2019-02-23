27744e17b5a6e8cc4a936af5842341a3aeb2db3f
root <root@.qh.com>
2019-02-23 14:36:47 +0800

    rm *

--- bak.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in /test/*.bak
-do
-echo `basename $i .bak`
-mv $i /test/`basename $i .bak`
-done
-
--- ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "ip " str
-[ -z $str ]&&exit
-if ping -c2 $str &>/dev/null
-then 
-	echo "alive"
-else
-	echo "not alive"
-fi
